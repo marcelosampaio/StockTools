71487a7b346d8f13c3eb19a81855b1721dd88337
Marcelo Sampaio <marcelosampaiio1965@gmail.com>
2024-07-24 11:34:17 -0300

    Initial Commit

---
+++ StockTools/StockToolsApp.swift
@@ -0,0 +1,17 @@
+//
+//  StockToolsApp.swift
+//  StockTools
+//
+//  Created by Marcelo Sampaio on 24/07/24.
+//
+
+import SwiftUI
+
+@main
+struct StockToolsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
